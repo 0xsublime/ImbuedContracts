ca7e72059dfe2e67a97830316860d0803ad9ee87
0xsublime <91898730+0xsublime@users.noreply.github.com>
2022-01-23 16:06:22 +0100

    add IImbuedNFT interface

--- src/ImbuedMinterV2.sol
+++ src/ImbuedMinterV2.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
+import "./IImbuedNFT.sol";
+
 contract ImbuedMinterV2 {
+    IImbuedNFT constant public NFT = IImbuedNFT(0x000001E1b2b5f9825f4d50bD4906aff2F298af4e);
 }
