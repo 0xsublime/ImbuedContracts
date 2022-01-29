6a06892664bc456c04604e517103ca2a9c534880
0xsublime <91898730+0xsublime@users.noreply.github.com>
2022-01-29 02:14:48 +0100

    3 different minting approaches, with gas costs

--- src/ImbuedMinterV2.sol
+++ src/ImbuedMinterV2.sol
@@ -7,26 +7,79 @@ import "./IImbuedNFT.sol";
 contract ImbuedMintV2 is Ownable {
     IImbuedNFT constant public NFT = IImbuedNFT(0x000001E1b2b5f9825f4d50bD4906aff2F298af4e);
 
+    uint16 maxWhiteListId = 99;
     uint16 public nextId = 101;
     uint16 public maxId = 199;
     uint256 whitelistPrice = 0.05 ether;
+
+    /// TODO: Pick how to do this. This is a naive and gas-efficient way
+    /// Whitelist only mint.
+    /// To save gas we use naive whitelisting
+    mapping (address => bool) address2claimed; // callers that have claimed.
+
+    function mintA(uint8 amount) external payable {
+        require(msg.value == amount * whitelistPrice, "wrong amount of ether sent");
+
+        require(!address2claimed[msg.sender], "sender already claimed");
+        uint256 bal = NFT.balanceOf(msg.sender);
+        require(bal >= amount, "mint amount too large");
+
+
+       address2claimed[msg.sender] = true;
+        _mint(msg.sender, amount);
+    }
+
+    /// TODO: Pick how to do this. This is a more exact way.
+    // TODO Ensure only tokens of lower than X can be used for whitelist
+    /// Whitelist only mint.
+    mapping (uint256 => bool) tokenid2claimed; // token ids that are claimed.
+
+    function mintB(uint8 amount) external payable {
+        require(msg.value == amount * whitelistPrice, "wrong amount of ether sent");
+
+        uint256 claimed = 0;
+        uint256 i = 0;
+        while (claimed < amount) {
+            uint256 id = NFT.tokenOfOwnerByIndex(msg.sender, i);
+            unchecked { i++; }
+            if (tokenid2claimed[id] || id > maxWhiteListId) {
+                continue;
+            }
+            tokenid2claimed[id] = true;
+            unchecked { claimed++; }
+        }
+        _mint(msg.sender, amount);
+    }
+
+    function mintC(uint16[] calldata tokenIds) external payable {
+        uint8 amount = uint8(tokenIds.length);
+        require(msg.value == amount * whitelistPrice, "wrong amount of ether sent");
+
+        unchecked {
+            for (uint256 i = 0; i < amount; i++) {
+                uint256 id = tokenIds[i];
+                require(msg.sender == NFT.ownerOf(id), "not allowed to mint for tokenId");
+                require(id <= maxWhiteListId, "not a whitelisted token id");
+                require(!tokenid2claimed[id], "token already used for claim");
+                tokenid2claimed[id] = true;
+            }
+        }
+        _mint(msg.sender, amount);
+    }
 
     // only owner
 
     function adminMintAmount(address recipient, uint8 amount) external onlyOwner() {
         require(amount + nextId <= maxId, "can't mint that many");
-        uint256 nextCache = nextId;
-        unchecked {
-            for (uint256 i = 0; i < amount; i++) {
-                NFT.mint(recipient, nextCache);
-                nextCache++;
-            }
-        }
-        nextId += amount;
+        _mint(recipient, amount);
     }
 
     function adminMintSpecific(address recipient, uint256 tokenId) external onlyOwner() {
         NFT.mint(recipient, tokenId);
+    }
+
+    function setMaxWhitelistId(uint16 newMaxWhitelistId) external onlyOwner() {
+        maxWhiteListId = newMaxWhitelistId;
     }
 
     function setNextId(uint16 newNextId) external onlyOwner() {
@@ -39,5 +92,28 @@ contract ImbuedMintV2 is Ownable {
     
     function setWhitelistPrice(uint256 newPrice) external onlyOwner() {
         whitelistPrice = newPrice;
+    }
+
+    function kill() external onlyOwner() {
+        selfdestruct(payable(msg.sender));
+    }
+
+    function withdrawAll(address payable recipient) external onlyOwner() {
+        recipient.call{value: address(this).balance}("");
+    }
+
+    // internal
+
+    // TODO: reentrancy?
+    function _mint(address recipient, uint8 amount) internal {
+        uint256 nextCache = nextId;
+        require(nextCache + amount <= maxId, "can't mint that many");
+        unchecked {
+            for (uint256 i = 0; i < amount; i++) {
+                NFT.mint(recipient, nextCache); // reentrancy danger. Handled by fact that same ID can't be minted twice.
+                nextCache++;
+            }
+        }
+        nextId += amount;
     }
 }
--- src/ImbuedMinterV2.t.sol
+++ src/ImbuedMinterV2.t.sol
@@ -5,14 +5,22 @@ import "ds-test/test.sol";
 
 import "./ImbuedMinterV2.sol";
 
+import "openzeppelin-contracts/token/ERC721/utils/ERC721Holder.sol";
+
 contract ImbuedMinterV2Test is DSTest {
     ImbuedMintV2 minter;
     IImbuedNFT NFT;
+
+    User user;
 
     function setUp() public {
         minter = new ImbuedMintV2();
         NFT = minter.NFT();
         NFT.setMintContract(address(minter));
+        user = new User();
+        payable(user).transfer(10 ether);
+        minter.adminMintAmount(address(user), 5);
+        minter.setMaxWhitelistId(105);
     }
 
     function test_accessNFT() public {
@@ -25,7 +33,7 @@ contract ImbuedMinterV2Test is DSTest {
 
     function test_adminMinorMint() public {
         uint16 nextId = minter.nextId();
-        uint8 amount = 50; // uint8(minter.maxId() - nextId);
+        uint8 amount = 3; // uint8(minter.maxId() - nextId);
         minter.adminMintAmount(address(1), amount);
         assertEq(NFT.balanceOf(address(1)), amount);
         assertEq(minter.nextId(), nextId + amount);
@@ -36,6 +44,30 @@ contract ImbuedMinterV2Test is DSTest {
         assertEq(NFT.ownerOf(100), address(2));
     }
 
+    function test_gasCostMintA() public {
+        user.mintA(minter);
+    }
+
+    function test_gasCostMintB() public {
+        user.mintB(minter);
+    }
+
+    function test_gasCostMintC() public {
+        user.mintC(minter);
+    }
+
+    function test_gasCostMintA5() public {
+        user.mintA5(minter);
+    }
+
+    function test_gasCostMintB5() public {
+        user.mintB5(minter);
+    }
+
+    function test_gasCostMintC5() public {
+        user.mintC5(minter);
+    }
+
     function testFail_basic_sanity() public {
         assertTrue(false);
     }
@@ -43,4 +75,41 @@ contract ImbuedMinterV2Test is DSTest {
     function test_basic_sanity() public {
         assertTrue(true);
     }
+}
+
+contract User is ERC721Holder {
+
+    function mintA(ImbuedMintV2 minter) external {
+        minter.mintA{value: 0.05 ether * 1}(1);
+    }
+
+    function mintB(ImbuedMintV2 minter) external {
+        minter.mintB{value: 0.05 ether * 1}(1);
+    }
+
+    function mintC(ImbuedMintV2 minter) external {
+        uint16[] memory ids = new uint16[](1);
+        ids[0] = 101;
+        minter.mintC{value: 0.05 ether * 1}(ids);
+    }
+
+    function mintA5(ImbuedMintV2 minter) external {
+        minter.mintA{value: 0.05 ether * 5}(5);
+    }
+
+    function mintB5(ImbuedMintV2 minter) external {
+        minter.mintB{value: 0.05 ether * 5}(5);
+    }
+
+    function mintC5(ImbuedMintV2 minter) external {
+        uint16[] memory ids = new uint16[](5);
+        ids[0] = 101;
+        ids[1] = 102;
+        ids[2] = 103;
+        ids[3] = 104;
+        ids[4] = 105;
+        minter.mintC{value: 0.05 ether * 5}(ids);
+    }
+
+    receive() external payable {}
 }
